6314a29a4dad2ac33ffbea45f4d74c14198b31b2
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-08-17 18:39:49 +0800

    sort

--- core.js
+++ core.js
@@ -157,7 +157,7 @@ Q.core = Q.Evented.extend({
 		this.bullets = [];
 		this.weapons = [];
 		this.terrain = [];
-		this.genwpn={cur:0,max:2100};
+		this.genwpn={cur:0,max:2400};
 		this.generate_terrain();
 		this.renderer = new Q.renderer(enviroment,size,block_size,this.terrain);
 		this.running = false;
@@ -435,7 +435,7 @@ Q.core = Q.Evented.extend({
 		if (this.genwpn.cur>=this.genwpn.max) {
 			this.generate_weapon();
 			this.genwpn.cur=0;
-			this.genwpn.max+=10;
+			this.genwpn.max+=600;
 		}
 	},
 
