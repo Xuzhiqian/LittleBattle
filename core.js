a42d067033aaeab4deef9edef26e75bf7249f8b2
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-08-23 14:05:04 +0800

    score 5.0

--- core.js
+++ core.js
@@ -309,7 +309,7 @@ Q.core = Q.Evented.extend({
 				if (id === winner_id)
 					this.stat[id].d_score = Math.round(Math.max(0, f_score(score1/score0))*(k+o)/(d));
 				else
-					this.stat[id].d_score = Math.round(Math.min(0,-f_score(score0/score1))+4*(k+o)/d);
+					this.stat[id].d_score = Math.round(Math.min(0,-f_score(score0/score1)*Math.log10(score0))+5*(k+o)/d);
 			}
 	},
 
