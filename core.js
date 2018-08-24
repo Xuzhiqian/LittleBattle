21df7995e4a26d2f76eb862f1c5c0e667ff05148
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-08-25 02:27:32 +0800

    arsenal 2.1

--- core.js
+++ core.js
@@ -5,7 +5,7 @@ var Q = Quisus();
 var global_width,global_height;
 var weapons = ['Vector','Micro_Uzi','AKM','Scar-L','M416','Groza','Kar-98K','AWM','S1897','S686','M249','Minigun','Pan'];
 var tools = ['clone','heal','invisible','bounce','jet','gravity'];
-var descs = {'ghost':0,'derivative',1};
+var descs = {'ghost':0,'derivative':1};
 
 var v_a=function (a, b) {
 		return {x: a.x + b.x, y: a.y + b.y}
