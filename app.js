10bec36a66a7cdfbb3ece80f40c00199a0435560
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-09-06 13:55:27 +0800

    disc fix

--- app.js
+++ app.js
@@ -6,13 +6,34 @@ var io = require("socket.io").listen(server);
 var mysql = require("mysql");
 
 //连接数据库
-var connection = mysql.createConnection({
+var config = mysql.createConnection({
 	host		: 'localhost',
 	user 		: 'root',
 	password	: '990212',
 	database	: 'littlebattle'
 });
-connection.connect();
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(config); 	  // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
 
 //响应请求
